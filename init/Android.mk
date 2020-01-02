dd704d6a2e13f37441ea733aca5319a43672f6c6
Mateo Trueba <mateo.trueba@gmail.com>
2020-01-02 18:29:51 -0300

    Use exynos7870-common sepolicy for now From this repo: https://github.com/Exynos7870/android_device_samsung_exynos7870-common (lineage-15.1 branch)
    Contains some fixes to build errors
    
    Signed-off-by: Mateo Trueba <mateo.trueba@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -2,8 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+	system/core/base/include \
+	system/core/init
 LOCAL_CFLAGS := -Wall
 LOCAL_SRC_FILES := init_gteslte.cpp
 LOCAL_MODULE := libinit_gteslte
+LOCAL_STATIC_LIBRARIES += libbase
 include $(BUILD_STATIC_LIBRARY)

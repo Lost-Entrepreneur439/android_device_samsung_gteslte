fb66607df46c4698f53f7e4241fcf74ccb5cd327
imranpopz <imranpopz32@gmail.com>
2020-01-19 12:46:52 -0300

    j2lte: build Audio HAL
    Signed-off-by: Mateo Trueba <mateo.trueba@gmail.com>

--- device.mk
+++ device.mk
@@ -151,6 +151,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
     
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+
+PRODUCT_PACKAGES += \
     audio.primary.universal3475 \
     audio.a2dp.default \
     audio.usb.default \

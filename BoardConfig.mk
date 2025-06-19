a8ce14523e108938a44914f9a7f5e364bf282ccf
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-06-18 22:36:27 -0230

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a7
 
 # Extracted with libbootimg
-BOARD_MKBOOTIMG_ARGS := -kernel_offset 0x00008000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048

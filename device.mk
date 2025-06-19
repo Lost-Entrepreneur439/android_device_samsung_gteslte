2a0de7914cfe632b60ef4227aea492d3ba5711b9
Ivy <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2025-06-18 22:50:11 -0230

    j2lte: build Graphics HAL

--- device.mk
+++ device.mk
@@ -83,6 +83,11 @@ PRODUCT_COPY_FILES += \
 # Gralloc
 PRODUCT_PACKAGES += \
     gralloc.exynos3 \
+		android.hardware.graphics.allocator@2.0-impl \
+		android.hardware.graphics.allocator@2.0-service \
+		android.hardware.graphics.composer@2.1-impl \
+		android.hardware.graphics.mapper@2.0-impl \
+		android.hardware.memtrack@1.0-impl \
     libion \
     libfimg
 
